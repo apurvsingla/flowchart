43203ad4e79140900e0a21be4734133876c3a550
Apurv <apurvsingla@gmail.com>
2020-10-07 14:55:54 +0530

    correct

--- src/Components/SwitchButton/SwitchButton.jsx
+++ src/Components/SwitchButton/SwitchButton.jsx
@@ -5,8 +5,36 @@ import './SwitchButton.scss';
 export default function SwitchButton(props) {
     return (
         <Switch type="checkbox" 
-        checkedIcon={false} 
-        uncheckedIcon={false} 
+        checkedIcon={
+            <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          fontSize: 10,
+          color: "white",
+          paddingLeft: 12,
+        }}
+      >
+        Output
+      </div>
+        } 
+        uncheckedIcon={
+            <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          fontSize: 10,
+          color: "white",
+          paddingRight: 10,
+        }}
+      >
+        Input
+      </div>
+        } 
         onColor='#f8c01d'
         offColor='#5095ce'
         height={18}
